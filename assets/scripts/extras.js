75499931cc468f1995a437b28c8701d08d01b476
Jimmi Kristensen <jikr@tv2.dk>
2017-12-22 15:21:29 +0200

    added font awesome

--- assets/scripts/extras.js
+++ assets/scripts/extras.js
@@ -2,7 +2,7 @@ $(function() {
   $(extraButtons).each(function(i, v) {
     console.log('NODE', i, v);
     $("<a>", {
-      class: "btn-extras-home animated bounceIn",
+      class: "btn-subscribe-home animated bounceIn",
       id: "extra-btn-"+i,
       href: v.url,
       text: v.name
