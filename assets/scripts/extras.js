291692991370eb7208e4396d464d4aa3f52c7ee3
Jimmi Kristensen <jikr@tv2.dk>
2017-12-22 19:45:18 +0200

    added custom home buttons

--- assets/scripts/extras.js
+++ assets/scripts/extras.js
@@ -1,11 +1,22 @@
 $(function() {
   $(extraButtons).each(function(i, v) {
     console.log('NODE', i, v);
-    $("<a>", {
+    var link = $("<a>", {
       class: "btn-subscribe-home animated bounceIn",
       id: "extra-btn-"+i,
       href: v.url,
       text: v.name
-    }).appendTo("#btn-panel-home");
+    });
+    console.log('LINK', link);
+    if (v.icon) {
+      document.styleSheets[0].addRule(link.id+'::before','content: "'+v.icon+'";');
+    }
+    if (v.target) {
+      link.attr("target", v.target);
+    }
+    if (v.title) {
+      link.attr("title", v.title);
+    }
+    link.appendTo("#btn-panel-home")
   });
 });
