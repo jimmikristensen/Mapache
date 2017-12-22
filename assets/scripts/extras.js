b1997c71b3206fa145a136a9f0f7d15dabdf4123
Jimmi Kristensen <jikr@tv2.dk>
2017-12-22 11:24:08 +0200

    adding functionality for multiple button on frontpage

--- assets/scripts/extras.js
+++ assets/scripts/extras.js
@@ -1,12 +1,10 @@
 $(function() {
   $(extraButtons).each(function(i, v) {
     console.log('NODE', i, v);
-    $("cover-wrap-content").append(
-      $("<a>", {
-        class: "btn-subscribe-home animated bounceIn",
-        id: "extra-btn-"+i,
-        text: v.name
-      })
-    );
+    $("<a>", {
+      class: "btn-subscribe-home animated bounceIn",
+      id: "extra-btn-"+i,
+      text: v.name
+    }).appendTo("#cover-wrap-content");
   });
 });
