dfdc2acfffc9e4613da4745197d198fc4d3c87a2
Jimmi Kristensen <jikr@tv2.dk>
2017-12-23 16:59:31 +0200

    changed name of buttons variable

--- assets/scripts/extras.js
+++ assets/scripts/extras.js
@@ -1,20 +1,22 @@
 $(function() {
-  $(extraButtons).each(function(i, v) {
-    var link = $("<a>", {
-      class: "btn-subscribe-home animated bounceIn",
-      id: "extra-btn-"+i,
-      href: v.url,
-      text: v.name
+  if (homeScreenButtons) {
+    $(homeScreenButtons).each(function (i, v) {
+      var link = $("<a>", {
+        class: "btn-subscribe-home animated bounceIn",
+        id: "hs-btn-" + i,
+        href: v.url,
+        text: v.name
+      });
+      if (v.class) {
+        link.addClass(v.class);
+      }
+      if (v.target) {
+        link.attr("target", v.target);
+      }
+      if (v.title) {
+        link.attr("title", v.title);
+      }
+      link.appendTo("#btn-panel-home")
     });
-    if (v.class) {
-      link.addClass(v.class);
-    }
-    if (v.target) {
-      link.attr("target", v.target);
-    }
-    if (v.title) {
-      link.attr("title", v.title);
-    }
-    link.appendTo("#btn-panel-home")
-  });
+  }
 });
